0005f282c39093b2a1279aac2b82473a5fc71764
pascode47 <paschalbiz47@gmail.com>
2025-06-19 11:08:35 +0300

    deployed version

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.use((req, res) => {
   });
 });
 
-// Start server
+ // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
